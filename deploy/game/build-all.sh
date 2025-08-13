069575271754048dcce76f4402c7369018670c2c
Farmerbilly27 <dmw2796@gmail.com>
2025-08-12 23:35:07 -0500

    add hit and run

--- deploy/game/build-all.sh
+++ deploy/game/build-all.sh
@@ -1,8 +1,10 @@
 # Harvest Moon
-./build-cloud.sh "gamecube" "G4AEE9"
-# Worms
-./build-cloud.sh "gamecube" "GWMP8P"
-# Zelda WW
-./build-cloud.sh "gamecube" "GZLE01"
+# ./build-cloud.sh "gamecube" "G4AEE9"
+# # Worms
+# ./build-cloud.sh "gamecube" "GWMP8P"
+# # Zelda WW
+# ./build-cloud.sh "gamecube" "GZLE01"
 # KAR
-./build-cloud.sh "gamecube" "GKYE01"
+# ./build-cloud.sh "gamecube" "GKYE01"
+# Simpsons H&R
+./build-cloud.sh "gamecube" "GHQE7D"
